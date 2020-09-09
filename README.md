7349c1527a4c7e83470b2a2381845b723f9da262
WSINTRA <will.simpson85@gmail.com>
2020-09-09 18:09:01 -0400

    completed step 2, half way through step 3

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-###TASK
+### TASK
 
 >Write a program that gets the top 50 trending movie torrents for the week and creates a playlist of youtube trailers.
 
--- main.js
+++ main.js
@@ -7,11 +7,56 @@
 
 //Step 1
 //Create a new puppeteer instance that connects to https://1337x.to/trending/w/movies/
+const puppeteer = require('puppeteer');
 
-//TEST
+async function run() {
 
-//Step 2
-//Parse the returning data and find the table that contains the torrent titles
+    const browser = await puppeteer.launch({ headless: true }); // default is true
+    const page = await browser.newPage();
+
+    await page.goto('https://1337x.to/trending/w/movies/');
+    //TEST
+    // await page.screenshot({ path: 'testScreener.png' });
+
+    //Step 2
+    //Parse the returning data and find the table that contains the torrent titles
+
+    //     <td class="coll-1 name">
+    // <div class="rating-box" style="padding-top: 2px;">
+    // <span class="active"><i class="flaticon-arw-right grey"></i></span>
+    // <span><i class="flaticon-arw-right"></i></span>
+    // <span class="rating-text" style="margin-top: 2px;">1</span>
+    // <span><i class="flaticon-arw-right"></i></span>
+    // </div>
+    // <a href="/torrent/4611461/Mulan-2020-1080p-WEBRip-x264-AAC5-1-RARBG/">Mulan.2020.1080p.WEBRip.x264.AAC5.1-RARBG</a> </td>
+
+    let torrentTable = 'body > main > div > div > div.featured-list.trending-torrent > div.table-list-wrap > table > tbody'
+
+    //Step 3
+    //Create an array of all unique titles, since many titles will be similar except for the scene tags, filter by the first 4 letters
+
+
+    let tableLen = 50;
+    let titleArray = []
+    for (let i = 0; i < tableLen; i++) {
+        let index = i;
+        let torrentTitle = `body > main > div > div > div.featured-list.trending-torrent > div.table-list-wrap > table > tbody > tr:nth-child(${index}) > td.coll-1.name > a`
+        titleArray.push(await page.evaluate((sel) => {
+            let element = document.querySelector(sel);
+            return element ? element.innerText : null;
+        }, torrentTitle))
+    }
+
+    //TEST
+    console.log(titleArray)
+
+
+    browser.close();
+}
+
+run()
+
+
 
 //TEST
 
