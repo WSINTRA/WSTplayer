a2df1b45c028099cea374afca08de4b724263f0a
WSINTRA <will.simpson85@gmail.com>
2020-09-11 17:22:08 -0400

    Still working with the YouTube API, struggling with quotas and permissions

--- SearchSample.js
+++ SearchSample.js
@@ -26,21 +26,21 @@ const youtube = google.youtube('v3');
 
 
 // a very simple example of searching for youtube videos
-function runSample(searchParam) {
-    var searchTerm = searchParam
+async function runSample(searchParamArray) {
+    console.log(searchParamArray, "Array is in module")
     fs.readFile('client_secret.json', async function processClientSecrets(err, content) {
         if (err) {
             console.log('Error loading client secret file: ' + err);
             return;
         }
         // Authorize a client with the loaded credentials, then call the YouTube API.
-        return authorize(JSON.parse(content), search);
+        authorize(JSON.parse(content), search);
     });
 
     function authorize(credentials, callback) {
-        var clientSecret = credentials.web.client_secret;
-        var clientId = credentials.web.client_id;
-        var redirectUrl = credentials.web.redirect_uris[0];
+        var clientSecret = credentials.installed.client_secret;
+        var clientId = credentials.installed.client_id;
+        var redirectUrl = credentials.installed.redirect_uris[0];
         var oauth2Client = new OAuth2(clientId, clientSecret, redirectUrl);
         // Check if we have previously stored a token.
         fs.readFile(TOKEN_PATH, function(err, token) {
@@ -48,15 +48,38 @@ function runSample(searchParam) {
                 getNewToken(oauth2Client, callback);
             } else {
                 oauth2Client.credentials = JSON.parse(token);
-                return callback(oauth2Client);
+                callback(oauth2Client);
             }
         });
     }
     async function search(auth) {
         var service = google.youtube('v3');
         google.options({ auth });
+        let trailerIDs = []
 
-        // var results = await service.search.list({ part: 'id,snippet', q: `${searchTerm}`, maxResults: 1 });
+        // trailerIDs = searchParamArray.map(searchTerm => await service.search.list({ part: 'id,snippet', q: `${searchTerm}`, maxResults: 1 }).data.items[0].id)
+
+        // const list = [1, 2, 3, 4, 5] //...an array filled with values
+
+        const functionWithPromise = (searchTerm) => { //a function that returns a promise
+            return service.search.list({ part: 'id,snippet', q: `${searchTerm}`, maxResults: 1 })
+        }
+
+        const anAsyncFunction = async item => {
+            return functionWithPromise(item)
+        }
+
+        const getData = async() => {
+            return Promise.all(searchParamArray.map(item => anAsyncFunction(item)))
+        }
+
+        getData().then(data => {
+                trailerIDs = data.filter(res => res.data.items[0])
+
+                console.log(trailerIDs)
+            })
+            // var results = await service.search.list({ part: 'id,snippet', q: `${searchTerm}`, maxResults: 1 });
+
         // let videoForPlayList = results.data.items[0].id.videoId
         // let currentPlaylist = await service.playlists.list({
         //     "part": [
@@ -69,19 +92,20 @@ function runSample(searchParam) {
         //Step 5
         //Create a new playlist using the youtube API
 
-        try {
-            let newPlaylist = await service.playlists.insert({
-                part: [
-                    "snippet"
-                ],
-                snippet: {
-                    title: "Trending This week"
-                }
-            });
-            console.log(newPlaylist)
-        } catch (err) {
-            console.log(err)
-        }
+        // try {
+        //     let todaysDate = (new Date()).toLocaleDateString().split("/").join("/")
+        //     let newPlaylist = await service.playlists.insert({
+        //         part: [
+        //             "snippet"
+        //         ],
+        //         snippet: {
+        //             title: `Trending Movies this week ${todaysDate}`
+        //         }
+        //     });
+        //     console.log(newPlaylist)
+        // } catch (err) {
+        //     console.log(err)
+        // }
 
         //TEST
 
@@ -92,7 +116,7 @@ function runSample(searchParam) {
         //Insert each top search result into the newly created playlist
 
     }
-
+    return "Done"
 }
 
 if (module === require.main) {
--- main.js
+++ main.js
@@ -11,45 +11,44 @@ const puppeteer = require('puppeteer');
 const youTubeSearch = require('./SearchSample');
 //ptn is a torrent name parse from https://github.com/jzjzjzj/parse-torrent-name
 var ptn = require('parse-torrent-name');
-const CREDS = require('./cred');
 
 async function run() {
 
-    // const browser = await puppeteer.launch({ headless: true }); // default is true
-    // const page = await browser.newPage();
+    const browser = await puppeteer.launch({ headless: true }); // default is true
+    const page = await browser.newPage();
 
-    // await page.goto('https://1337x.to/trending/w/movies/');
-    // //TEST
-    // // await page.screenshot({ path: 'testScreener.png' });
+    await page.goto('https://1337x.to/trending/w/movies/');
+    //TEST
+    // await page.screenshot({ path: 'testScreener.png' });
 
-    // //Step 2
-    // //Parse the returning data and find the table that contains the torrent titles
+    //Step 2
+    //Parse the returning data and find the table that contains the torrent titles
 
-    // let torrentTable = 'body > main > div > div > div.featured-list.trending-torrent > div.table-list-wrap > table > tbody'
+    let torrentTable = 'body > main > div > div > div.featured-list.trending-torrent > div.table-list-wrap > table > tbody'
 
-    // //Step 3
-    // //Create an array of all unique titles, since many titles will be similar except for the scene tags, filter by the first 4 letters
-    // let tableLen = 50;
-    // let titleArray = []
-    // for (let i = 0; i < tableLen; i++) {
-    //     let index = i;
-    //     let torrentTitle = `body > main > div > div > div.featured-list.trending-torrent > div.table-list-wrap > table > tbody > tr:nth-child(${index}) > td.coll-1.name > a`
-    //     titleArray.push(await page.evaluate((sel) => {
-    //         let element = document.querySelector(sel);
-    //         return element ? element.innerText : null;
-    //     }, torrentTitle))
-    // }
-    // titleArray = titleArray.filter(el => el !== null)
-    // titleArray = titleArray.map(element => ptn(element))
+    //Step 3
+    //Create an array of all unique titles, since many titles will be similar except for the scene tags, filter by the first 4 letters
+    let tableLen = 3; //set this back to 50 when ready
+    let titleArray = []
+    for (let i = 0; i < tableLen; i++) {
+        let index = i;
+        let torrentTitle = `body > main > div > div > div.featured-list.trending-torrent > div.table-list-wrap > table > tbody > tr:nth-child(${index}) > td.coll-1.name > a`
+        titleArray.push(await page.evaluate((sel) => {
+            let element = document.querySelector(sel);
+            return element ? element.innerText : null;
+        }, torrentTitle))
+    }
+    titleArray = titleArray.filter(el => el !== null)
+    titleArray = titleArray.map(element => ptn(element))
 
-    // uniqeTitles = [...new Set(titleArray.map(el => (el.title + " trailer")))];
-    // //TEST
+    uniqeTitles = [...new Set(titleArray.map(el => (el.title + " trailer")))];
+    //TEST
     // console.log(uniqeTitles)
 
     //Step 4
     //Connect to the youTube API with credentials
 
-    let data = await youTubeSearch("Dogs");
+    let data = youTubeSearch(uniqeTitles);
     //TEST
     console.log(data);
 
